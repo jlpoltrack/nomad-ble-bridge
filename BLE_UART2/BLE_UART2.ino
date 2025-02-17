065ccccf6f7a1cf4ba755647423959c18b3e8ded
JLP <jlpoltrack@gmail.com>
2025-02-17 14:59:12 -0500

    much improved

--- BLE_UART2/BLE_UART2.ino
+++ BLE_UART2/BLE_UART2.ino
@@ -1,65 +1,56 @@
-/*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
-   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
-
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
-   In this example rxValue is the data received (only accessible inside that function).
-   And txValue is the data to be sent, in this example just a byte incremented every second. 
-*/
+// Modified version of example: https://github.com/espressif/arduino-esp32/blob/master/libraries/BLE/examples/UART/UART.ino
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-BLEServer *pServer = NULL;
-BLECharacteristic * pTxCharacteristic;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint8_t txValue = 0;
-
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
 
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
-#define MTU_SIZE 32. // seems to be ideal, 64 struggled to connect at times
+
+BLEServer *pServer = NULL;
+BLECharacteristic * pTxCharacteristic;
+bool deviceConnected = false;
+bool serialStarted = false;
+bool mtuChecked = false;
+uint16_t connId;
+uint16_t actualMTU;
 
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
-      Serial0.setRxBufferSize(8192);
-      Serial0.setTxBufferSize(8192);
-      Serial0.begin(230400, SERIAL_8N1, 20, 21);
+      connId = pServer->getConnId();
+      Serial.println("C");
     };
 
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
-      Serial0.end();
+      serialStarted = false;
+      mtuChecked = false;
+      Serial.println("D");
     }
 };
 
+
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
+      if (!serialStarted) {
+        serialStarted = true;
+
+        Serial0.setRxBufferSize(16384);  // really just for initial connection, actual use much less
+        Serial0.setTxBufferSize(512);    // allow for MAVFTP messages
+        //Serial0.begin(230400, SERIAL_8N1, 16, 17);  // for S3 board
+        Serial0.begin(115200, SERIAL_8N1, 20, 21); // for C3 bridge
+        Serial.println("S0");
+      }
+
       std::string rxValue = pCharacteristic->getValue();
-      if (rxValue.length() > 0) {
-        for (int i = 0; i < rxValue.length(); i++)
-          Serial0.write(rxValue[i]);
+      size_t payloadLength = rxValue.length();
+      if (payloadLength > 0) {
+        Serial0.write((uint8_t*)rxValue.c_str(), payloadLength);
       }
     }
 };
@@ -67,59 +58,79 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Setup Started");
 
-  // Create the BLE Device
+  // Create BLE Device, set MTU
   BLEDevice::init("mLRS BLE Bridge");
+  BLEDevice::setMTU(515);  // plenty of headroom
 
-  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P21);
+  // Set BLE Power, P12 = 12 dBm
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P12);
 
-  // Create the BLE Server
+  // Create BLE Server
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
+  
 
-  // Create the BLE Service
+  // Create BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Create a BLE Characteristic
+  // Create BLE Characteristics
   pTxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
   pTxCharacteristic->addDescriptor(new BLE2902());
   BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_WRITE_NR);
   pRxCharacteristic->setCallbacks(new MyCallbacks());
 
-  // Start the service
+  // Start Service
   pService->start();
 
-  // Set the MTU
-  BLEDevice::setMTU(MTU_SIZE + 16);  // plenty of headroom
-  
   // Start advertising
   pServer->getAdvertising()->start();
-  Serial.println("Waiting a client connection to notify...");
+  Serial.println("Setup Done, Advertising");
 }
 
 void loop() {
 
     if (deviceConnected) {
-        if (Serial0.available() > MTU_SIZE) {
-            uint8_t buffer[MTU_SIZE];
-            Serial0.readBytes(buffer, MTU_SIZE);
 
-            // Send the data over BLE
-            pTxCharacteristic->setValue(buffer, MTU_SIZE);
-            pTxCharacteristic->notify();
-        }
-	}
+      if (!mtuChecked) { // have to do here because if you try and get the MTU in the callback it always reports 23...
+        mtuChecked = true;
+        delay(500);
+        Serial.print("Conn ID from the onConnect Callback: ");
+        Serial.print(connId);
+        Serial.print(", MTU: ");
+        actualMTU = pServer->getPeerMTU(connId);      
+        Serial.println(actualMTU);
+      }
+       
+      if (Serial0.available() >= 32) {
+
+        // Calculate the number of bytes to read
+        uint16_t availableBytes = Serial0.available();
+        uint16_t bytesToRead = min(availableBytes, (uint16_t)(actualMTU - 3));  // Limit to negotiated MTU - 3 bytes for header
+
+        // Create a buffer to hold the data
+        uint8_t buffer[bytesToRead];
+        
+        // Read data into the buffer
+        Serial0.readBytes(buffer, bytesToRead);
+
+        // Send the data over BLE as a single chunk
+        pTxCharacteristic->setValue(buffer, bytesToRead);
+        pTxCharacteristic->notify();
+
+        //Serial.println(bytesToRead);
+      }
+
+
+    }
 
     // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
+    if (!deviceConnected) {
         delay(500); // give the bluetooth stack the chance to get things ready
         pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-		// do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
+        Serial0.end();
+        Serial.println("Advertising");
+        delay(4500); // less spam
     }
 }
